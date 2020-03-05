36839b7563cf0e987927d4ec21aa9525dd72971e
mymaze04 <mazemafer@gmail.com>
2020-03-05 13:22:36 +0800

    adding files

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body id="home-page">
+        
+          <div class="container">
+        <header class="header">
+            <a class="link-title" href="/">Azure Activity 3</a>
+        </header>     
+
+        <nav class="menu">  
+            <ul>
+                <li><a href="index.html" class="active">Home</a></li>
+                <!--<li>  <a href="fileManager.php">File Manager</a></li>-->
+              
+            </ul>
+        </nav>    
+
+<!-- <aside>
+<h1>Side Bar</h1>    
+</aside>     -->
+          <form action="https://alegrebuenaflorfernandezortiz.azurewebsites.net/api/RecordPayroll" method="get" enctype="multipart/form-data" >
+            
+            <label>Employee Name : </label>
+            <input type="text" name="empname"> <br><br>
+            <label>Department    : </label>
+            <input type="text" name="department"> <br><br>
+            <label>Position : </label>
+            <input type="text" name="position"> <br><br>
+             <label>Rate : </label>
+            <input type="text" name="rate"> <br><br>
+            <label>Worked Hours : </label>
+            <input type="text" name="workedhours"> <br><br>
+             <label>Deductions : </label>
+            <input type="text" name="deductions"> <br><br>
+           
+           <input type="submit">
+
+           
+          </form>
+
+
+        <!-- <article class="main">
+            
+                    
+           
+             <div id="home-image">
+            </div> 
+        </article> -->
+
+        <br><br><br><br><br><br><br><br><br><br><br><br>
+        <footer class="footer">
+        
+        &copy; 2020 Image Uploading Website &nbsp;<span class="separator">|</span> Design by:<a href="http://www.facebook.com/mymaze04">Mark Fernandez</a>    
+        </footer>    
+
+
+    </div>
+
+</body>
+
+</html>
