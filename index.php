27352437169c90b87c3456281500731f4fa96b02
mymaze04 <49851566+mymaze04@users.noreply.github.com>
2020-03-05 15:51:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -22,14 +22,14 @@
 <!-- <aside>
 <h1>Side Bar</h1>    
 </aside>     -->
-          <form action="https://alegrebuenaflorfernandezortiz.azurewebsites.net/api/RecordPayroll" method="get" enctype="multipart/form-data" >
+          <form action="#" method="get" enctype="multipart/form-data" >
             
-            <label>Employee Name : </label>
-            <input type="text" name="empname"> <br><br>
-            <label>Department    : </label>
-            <input type="text" name="department"> <br><br>
-            <label>Position : </label>
-            <input type="text" name="position"> <br><br>
+            <label>Product Name : </label>
+            <input type="text" name="txt_productname"> <br><br>
+            <label>Color    : </label>
+            <input type="text" name="txt_color"> <br><br>
+            <label>Price : </label>
+            <input type="text" name="txt_price"> <br><br>
              <label>Rate : </label>
             <input type="text" name="rate"> <br><br>
             <label>Worked Hours : </label>
@@ -63,3 +63,21 @@
 </body>
 
 </html>
+<?php
+$host = 'mydemoserver.mysql.database.azure.com';
+$username = 'myadmin@mydemoserver';
+$password = 'your_password';
+$db_name = 'your_database';
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+
+//Close the connection
+mysqli_close($conn);
+?>
